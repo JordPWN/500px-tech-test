be2c2c361d7aa086dadc6731545e143192cb0a2c
JordPWN <jordan@kineticcafe.com>
2020-09-03 13:46:56 -0400

    Add 'react-justified-layout' and begin styling

--- src/components/album/Album.js
+++ src/components/album/Album.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import JustifiedLayout from 'react-justified-layout';
 
-import Photo from '../common/Photo.js'
+import Photo from '../common/Photo.js';
 
 // import '../styles/Album.scss';
-
 
 export default class Album extends React.Component {
   constructor(props) {
@@ -12,17 +12,29 @@ export default class Album extends React.Component {
       photos: props.photos
     };
   }
+
+  photosLoaded(photos) {
+    if (this.props.photos) {
+      return <JustifiedLayout className="album-container">
+          { photos }
+        </JustifiedLayout>
+    } else {
+      return "No photos!"
+    }
+  }
   
   render() {
     const photos = this.props.photos?.map(
-      photo => <Photo photo={photo} />
+      photo => <Photo photo={photo} key={photo?.id}
+        aspectRatio={photo.width / photo.height} />
     )
+
     return (
       <div className="album">
-        <div className="album-container">
-          { photos }
+          <JustifiedLayout className="album-container" containerPadding={80} containerWidth={document.documentElement.clientWidth}>
+            { photos }
+          </JustifiedLayout>
         </div>
-      </div>
     );
   }
 }
--- src/components/common/Photo.js
+++ src/components/common/Photo.js
@@ -2,14 +2,11 @@ import React from 'react';
 
 // import '../styles/Photo.scss';
 
-
 export default class Photo extends React.Component {
   render() {
     return (
-      <div className="photo">
-        <div className="photo-container">
-          <img src={this.props.photo.image_url} alt='' />
-        </div>
+      <div className="photo" style={this.props.style}>
+        <img src={this.props.photo.image_url} alt={this.props.photo.name} />
       </div>
     );
   }
